21750629a95fac6f53841c6ad22510893c999805
condebyte <manuelvillamar97@gmail.com>
2023-11-20 20:03:56 +0100

    cambios de fragment

---
+++ app/src/main/java/com/condex/pmdmpractica1/ui/adapters/FragmentAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.prueba.ui.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class FragmentAdapter (
+    fragmentManager: FragmentManager,
+    lifecycle: Lifecycle
+) : FragmentStateAdapter(fragmentManager, lifecycle) {
+
+    private val mFragmentList = ArrayList<Fragment>()
+    private val mFragmentTitleList = ArrayList<String>()
+
+    override fun getItemCount(): Int {
+        return mFragmentList.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return mFragmentList[position]
+    }
+
+    fun AddFragment(fragment: Fragment, title:String){
+        mFragmentList.add(fragment)
+        mFragmentTitleList.add(title)
+    }
+
+    fun getPageTitle(position: Int):String{
+        return mFragmentTitleList[position]
+    }
+
+
+}
